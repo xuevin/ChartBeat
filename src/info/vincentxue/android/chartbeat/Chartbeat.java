0be61e5abddcf298894b631363be8eaf8e415b78
Vincent <xue.vin@gmail.com>
2011-09-08 00:34:27 +0100

    Putting Chartbeat app for open source

---
+++ src/info/vincentxue/android/chartbeat/Chartbeat.java
@@ -0,0 +1,15 @@
+package info.vincentxue.android.chartbeat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Chartbeat extends Activity {
+	
+	/** Called when the activity is first created. */
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+}
